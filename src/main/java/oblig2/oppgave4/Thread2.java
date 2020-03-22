f3846fb4445abdfec9433606d6df7a1014297392
anafvana <56302061+anafvana@users.noreply.github.com>
2020-03-22 23:28:19 +0100

    Should I do this without serialisation?

--- src/main/java/oblig2/oppgave4/Thread2.java
+++ src/main/java/oblig2/oppgave4/Thread2.java
@@ -5,25 +5,40 @@ import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.nio.file.Paths;
 
 public class Thread2 extends Task<Save>{
     @Override
-    public Save call() throws Exception{
-        Save importedSave = null;
-        FileChooser fc = new FileChooser();
-        String currentDir = Paths.get(".").toAbsolutePath().normalize().toString();
-        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("ser", "*.ser"));
-        fc.setInitialDirectory(new File(currentDir));
+    public Save call(){
+        try{
+            Save importedSave = null;
+            FileChooser fc = new FileChooser();
+            String currentDir = Paths.get(".").toAbsolutePath().normalize().toString();
+            fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("ser", "*.ser"));
+            fc.setInitialDirectory(new File(currentDir));
 
-        File selectedFile = fc.showOpenDialog(null);
+            File selectedFile = fc.showOpenDialog(null);
 
-        FileInputStream fis = new FileInputStream(selectedFile);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        importedSave = (Save) ois.readObject();
-        ois.close();
+            FileInputStream fis = new FileInputStream(selectedFile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            importedSave = (Save) ois.readObject();
+            ois.close();
 
-        return importedSave;
+            return importedSave;
+        }
+        catch(IOException ex){
+            System.out.println("IOException is caught");
+            return null;
+        }
+
+        catch(ClassNotFoundException ex){
+            System.out.println("ClassNotFoundException is caught");
+            return null;
+        }
+
     }
+
+
 }
