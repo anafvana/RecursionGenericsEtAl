aeab3e3a3c82812f883466368875d90ddd2c5c73
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2020-03-23 01:41:08 +0100

    Added composite class to project (forgot previously :D)

--- src/main/java/Oppgave3/DesignMønster.java
+++ src/main/java/Oppgave3/DesignMønster.java
@@ -24,6 +24,26 @@ public class DesignMønster {
 // som hvis ett objekt blir endret så blir dets avhengige objekter oppdatert om det umiddelbart.
 // I vårt eksempel når det kommer til museklikk vil den da kunne oppdatere tegningen som blir gjort av draw() metoden.
 
+
+// Oppgave 3.3.3
+// Et Composite designmønster blir brukt når du vil selektivt behandle en gruppe med objekter som er en del av et hierarki som om
+// de er like, når de i realiteten ikke er like. Som i vårt eksempel kan vi ha masse forskjellige former som er rektangler og sirkler og alt mulig og
+// de vil alle bli sett på som ett type objekt.
+
+// I vårt eksempel har vi interfacet "Shape" og to "Leaf" klasser som f.eks Rectangle og Circle.
+// Vår Composite klasse vil da være "Drawing" eller noe lignende. Der har vi instansiert List<Shape> shapeList og vi har også add / remove metoder
+// for objekter (rektangler eller sirkler) som skal legges til eller fjernes fra listen. Den abstrakte metoden draw() vil se slik ut:
+
+    /*
+    public void draw() {
+		for(Shape shape : shapeList)
+		{
+			shape.draw();
+		}
+	}
+     */
+
+
 // Oppgave 3.3.4
 // Dette kan implementeres med designmønsteret Decorator. Vi må først lage en abstrakt Decorator klasse ShapeDecorator
 // som implementerer Shape. Den kan du se i klassen ShapeDecorator i denne pakken.
@@ -46,6 +66,29 @@ public class DesignMønster {
 
         System.out.println("\nRectangle with blue border:");
         blueRectangle.draw();
+
+        System.out.println();
+
+        Shape circle2 = new Circle();
+        Shape rectangle = new Rectangle();
+        Shape rectangle2 = new Rectangle();
+
+        Drawing drawing = new Drawing();
+        drawing.add(circle2);
+        drawing.add(rectangle);
+        drawing.add(rectangle2);
+        drawing.add(blueRectangle);
+
+        System.out.println("Commencing drawing of several shapes:");
+        drawing.draw();
+        drawing.clear();
+        System.out.println();
+
+
+        drawing.add(circle);
+        drawing.add(blueCircle);
+        System.out.println("Drawing new shapes, circle with no border and circle with blue border:");
+        drawing.draw();
     }
 
 }
---
+++ src/main/java/Oppgave3/Drawing.java
@@ -0,0 +1,30 @@
+package Oppgave3;
+
+import java.util.ArrayList;
+
+public class Drawing implements Shape {
+
+    private ArrayList<Shape> shapeList = new ArrayList<>();
+
+    @Override
+    public void draw() {
+        for(Shape shape : shapeList)
+        {
+            shape.draw();
+            System.out.println();
+        }
+    }
+
+    public void add(Shape shape) {
+        this.shapeList.add(shape);
+    }
+
+    public void remove(Shape shape) {
+        shapeList.remove(shape);
+    }
+
+    public void clear() {
+        System.out.println("All shapes removed from drawing");
+        this.shapeList.clear();
+    }
+}
